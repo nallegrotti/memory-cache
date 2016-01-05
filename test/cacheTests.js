226aed8f9be3c699193be6ed71e1397967b6a6c8
Nicolás Allegrotti <nallegrotti@garbarino.com.ar>
2016-01-05 12:42:36 -0300

    quita los accesos directos al mapa interno en los tests y devuelve las variables de ayuda para intervalos de tiempo

--- test/cacheTests.js
+++ test/cacheTests.js
@@ -2,15 +2,21 @@ var expect = require('chai').expect,
     cache = require('../memory-cache'),
     timestamp = Date.now();
 
+var seconds = 1000, 
+	minutes = 60*seconds, 
+	hours = 60*minutes, 
+	days = 60*hours;
+
 describe('Cache', function(){
 	it('should read from cached values', function(){
-		cache.storage.exists = {value:1234, expires: timestamp + 2800000};
-		var execution = 0;
-		cache.read('exists', function(){ execution++ })
-			.then(function(d){
-				expect(execution).to.be.equals(0);
-				expect(d).to.be.equals(1234);
-			})
+		cache.write('exists', function(){return 1234}, 8*hours).then(function(){
+			var execution = 0;
+			cache.read('exists', function(){ execution++ })
+				.then(function(d){
+					expect(execution).to.be.equals(0);
+					expect(d).to.be.equals(1234);
+				})
+		})
 	});
 
 	it("should execute missed function if there's no cached value", function(){
@@ -22,24 +28,25 @@ describe('Cache', function(){
 
 	it("should execute missed function if cached key is expired", function(){
 		var execution = 0;
-		cache.storage.expired = {value: 1234, expires: timestamp - 360000};
-		cache.read('expired', function(){execution++; return 321;}).then(function(d){
-			expect(execution).to.be.equals(1);
-			expect(d).to.be.equals(321);
+		cache.write('expired', function(){return 1234}, -6*minutes).then(function(){
+			cache.read('expired', function(){execution++; return 321;}).then(function(d){
+				expect(execution).to.be.equals(1);
+				expect(d).to.be.equals(321);
+			})
 		})
 	});
 
 	it("should set parametrized expiration", function(){
-		cache.read('not_exists_custom_expire', function(){return 222;}, 7200000).then(function(){
-			expect(cache.storage.not_exists_custom_expire.expires).to.be.above(3600000)
-				.and.below(timestamp + 7200001);
+		cache.read('not_exists_custom_expire', function(){return 222;}, 2*hours).then(function(){
+			expect(cache.storage.not_exists_custom_expire.expires).to.be.above(1*hours)
+				.and.below(timestamp + 2*hours + 1*seconds);
 		})
 	});
 
 	it('should set 8 hours of default expiration for new keys', function(){
 		cache.write('forced', function(){return 'mock';}).then(function(){
-			expect(cache.storage.forced.expires).to.be.above(25200000)
-				.and.below(timestamp + 28800001);
+			expect(cache.storage.forced.expires).to.be.above(8*hours)
+				.and.below(timestamp + 8*hours + 1*seconds);
 		})
 	})
 })
